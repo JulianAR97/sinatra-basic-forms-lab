251276e2b914ea6a72f43b317876d46c7edc7da0
Julian <julianrosenthal@yahoo.com>
2020-12-21 19:45:58 -0800

    commit tree d3806ed4a6a59ad594691359f28fb009265bd2bc

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,10 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy
+    attr_accessor :name, :breed, :age
+
+    def initialize(attr_hash)
+        @name = attr_hash[:name]
+        @breed = attr_hash[:breed]
+        @age = attr_hash[:age]
+    end
+end
